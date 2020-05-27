caf701d460125eaafffa425be5e85309f01b1053
BangKiHyun <rlrlvh@naver.com>
2020-05-27 18:23:17 +0900

    BoardController 및 BoardService 추가, 게시글 리스트 뷰 작성

--- Spring-Boot-Community-Web/src/main/java/me/bang/springbootweb/SpringbootwebApplication.java
+++ Spring-Boot-Community-Web/src/main/java/me/bang/springbootweb/SpringbootwebApplication.java
@@ -1,7 +1,17 @@
 package me.bang.springbootweb;
 
+import me.bang.springbootweb.domain.Board;
+import me.bang.springbootweb.domain.User;
+import me.bang.springbootweb.domain.enums.BoardType;
+import me.bang.springbootweb.repository.BoardRepository;
+import me.bang.springbootweb.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class SpringbootwebApplication {
@@ -10,4 +20,30 @@ public class SpringbootwebApplication {
         SpringApplication.run(SpringbootwebApplication.class, args);
     }
 
+    // CommandLineRunner
+    // 애플리케이션 구동 후 특정 코드를 실행시키고 싶을 때 직접 구현하는 인터페이스
+    // 애플리케이션 구동 시 테스트 데이터를 함께 생성하여 데모 프로젝트를 실행/테스트하고 싶을 때 편리하다.
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository,
+                                    BoardRepository boardRepository) throws Exception {
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+                    .name("bang")
+                    .password("1234")
+                    .email("bang@gamil.com")
+                    .createdDate(LocalDateTime.now())
+                    .build());
+
+            IntStream.rangeClosed(1, 200).forEach(index ->
+                    boardRepository.save(Board.builder()
+                            .title("게시글" + index)
+                            .subTitle("순서" + index)
+                            .content("콘텐츠")
+                            .boardType(BoardType.FREE)
+                            .createdDate(LocalDateTime.now())
+                            .updatedDate(LocalDateTime.now())
+                            .user(user)
+                            .build()));
+        };
+    }
 }
