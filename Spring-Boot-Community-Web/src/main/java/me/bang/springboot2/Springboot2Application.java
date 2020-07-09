13b5006eda74e77e9fb785229b431d4d759c39b2
BangKiHyun <rlrlvh@naver.com>
2020-07-09 17:19:45 +0900

    feat: 페이지별 게시글 보여주는 기능 구현, CommandLineRunner 설정

--- Spring-Boot-Community-Web/src/main/java/me/bang/springboot2/Springboot2Application.java
+++ Spring-Boot-Community-Web/src/main/java/me/bang/springboot2/Springboot2Application.java
@@ -1,7 +1,17 @@
 package me.bang.springboot2;
 
+import me.bang.springboot2.domain.Board;
+import me.bang.springboot2.domain.User;
+import me.bang.springboot2.enums.BoardType;
+import me.bang.springboot2.repository.BoardRepository;
+import me.bang.springboot2.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class Springboot2Application {
@@ -10,4 +20,28 @@ public class Springboot2Application {
         SpringApplication.run(Springboot2Application.class, args);
     }
 
+    //ConmmandLineRunner: 애플리케이션 구동 후 특정 코드를 실행시키고 싶을 때 직접 구현하는 인터페이스, 테스트 데이터 함께 생성할때 좋음
+    @Bean
+    public CommandLineRunner runner(UserRepository userRepository, BoardRepository boardRepository) throws Exception {
+        return (args) -> {
+            User user = userRepository.save(User.builder()
+                    .name("bang")
+                    .password("test")
+                    .email("bang@gmail.com")
+                    .createDate(LocalDateTime.now())
+                    .build());
+
+            IntStream.rangeClosed(1, 200).forEach(id ->
+                    boardRepository.save(Board.builder()
+                            .title("게시글" + id)
+                            .subTitle("순서" + id)
+                            .content("컨텐츠")
+                            .boardType(BoardType.FREE)
+                            .createDate(LocalDateTime.now())
+                            .updateeDate(LocalDateTime.now())
+                            .user(user)
+                            .build())
+            );
+        };
+    }
 }
---
+++ Spring-Boot-Community-Web/src/main/java/me/bang/springboot2/service/BoardService.java
@@ -0,0 +1,29 @@
+package me.bang.springboot2.service;
+
+import me.bang.springboot2.domain.Board;
+import me.bang.springboot2.repository.BoardRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Page<Board> findBoardList(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ? 0 : pageable.
+                getPageNumber() - 1, pageable.getPageSize());
+        return boardRepository.findAll(pageable);
+    }
+
+    public Board findBoardById(Long id) {
+        return boardRepository.findById(id)
+                .orElse(new Board());
+    }
+}
