8373a005459282c21fa3d6e58e3ed13e59478962
BangKiHyun <rlrlvh@naver.com>
2020-07-12 17:53:16 +0900

    feat: OAuth2 기초 설정

--- Spring-Boot-Community-Web/src/main/java/me/bang/springboot2/domain/User.java
+++ Spring-Boot-Community-Web/src/main/java/me/bang/springboot2/domain/User.java
@@ -3,6 +3,7 @@ package me.bang.springboot2.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import me.bang.springboot2.enums.SocialType;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -29,16 +30,26 @@ public class User implements Serializable {
     private String email;
 
     @Column
+    private String principal;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private SocialType socialType;
+
+    @Column
     private LocalDateTime createDate;
 
     @Column
     private LocalDateTime updatedDate;
 
     @Builder
-    public User(String name, String password, String email, LocalDateTime createDate, LocalDateTime updatedDate) {
+    public User(String name, String password, String email, String principal,
+                SocialType socialType, LocalDateTime createDate, LocalDateTime updatedDate) {
         this.name = name;
         this.password = password;
         this.email = email;
+        this.principal = principal;
+        this.socialType = socialType;
         this.createDate = createDate;
         this.updatedDate = updatedDate;
     }
